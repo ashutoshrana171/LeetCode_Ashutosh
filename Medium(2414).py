e3b52a8b6c633ddfd0f80d030b25b6635bc8f859
Ashutosh <62750665+ashutoshrana171@users.noreply.github.com>
2023-03-18 23:28:46 -0400

    Create Medium(2414).py

---
+++ Medium(2414).py
@@ -0,0 +1,25 @@
+"""
+Q:
+An alphabetical continuous string is a string consisting of consecutive letters in the alphabet. In other words, it is any substring of the string "abcdefghijklmnopqrstuvwxyz".
+
+For example, "abc" is an alphabetical continuous string, while "acb" and "za" are not.
+Given a string s consisting of lowercase letters only, return the length of the longest alphabetical continuous substring.
+"""
+
+class Solution(object):
+    def longestContinuousSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        tmax = 1
+        start = 1
+        current = 1 
+        for i in range(1,len(s)):
+            if ord(s[i]) - ord(s[i-1]) == 1:
+                current+= 1
+                tmax = max(current,start)
+            else:
+                current = 1
+
+        return tmax
